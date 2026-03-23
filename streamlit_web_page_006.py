ac56f130d1be8b19992804937b618749a185edcf
Peter Hruska <46312949+xertep@users.noreply.github.com>
2026-03-23 20:15:40 +0100

    Add files via upload

--- streamlit_web_page_006.py
+++ streamlit_web_page_006.py
@@ -32,6 +32,8 @@ MOUNTAIN_FORECAST_TYPES = [
 ]
 
 mountains = [
+    ("VY", "Žďárské vrchy"),
+    ("ZL", "Javorníky a Bílé Karpaty"),
     ("CB", "Šumava a Novohradské hory"),
     ("HK", "Krkonoše"),
     ("LB", "Jizerské hory"),
@@ -40,8 +42,6 @@ mountains = [
     ("PL", "Český a Slavkovský les"),
     ("PU", "Orlické hory"),
     ("UL", "Krušné hory"),
-    ("VY", "Žďárské vrchy"),
-    ("ZL", "Javorníky a Bílé Karpaty"),
 ]
 
 # Colors
